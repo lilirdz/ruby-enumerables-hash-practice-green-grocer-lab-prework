305360ebef9bed1cd5a63d64aa7551dc86e014a4
Lili Rodriguez <lilimrod@gmail.com>
2020-03-28 23:44:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,6 +48,7 @@ def checkout(cart, coupons)
 
   total = 0
   counter = 0
+  while counter < final_cart.length
   new_cart.each do |name, hash|
     total += (hash[:price] * (hash[:count])
     total = 
