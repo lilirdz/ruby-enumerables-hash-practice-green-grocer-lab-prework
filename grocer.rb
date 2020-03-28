188dc0ea1c75dd400d57f7199f63e1209544dfde
Lili Rodriguez <lilimrod@gmail.com>
2020-03-28 23:23:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,13 +42,18 @@ cart
 end
 
 def checkout(cart, coupons)
-  total = 0 
-  new_cart = consolidate_cart(cart) 
-  coupon_cart = apply_coupons(new_cart, coupons) 
-  clearance_cart = apply_clearance(coupon_cart) 
-  clearance_cart.each do |item, attribute_hash| 
-    total += (attribute_hash[:price] * attribute_hash[:count])
-  end 
-  total = (total * 0.9) if total > 100
-  end
+  new_cart = consolidate_cart(cart)
+  apply_coupons(new_cart, coupons)
+  apply_clearance(new_cart)
+
+  total = 0
+    new_cart.each do |name, hash|
+      total += (hash[:price] * hash[:count])
+    end
+  
+  if total >= 100
+      total *= 0.9
+    end
+  
+    total
 end
